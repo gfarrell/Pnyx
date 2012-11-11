d39f2dced2caad798e199813a67cbd94225736a6
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-11 01:27:53 +0000

    Created base policy controller class

---
+++ application/controllers/policy.php
@@ -0,0 +1,10 @@
+<?php
+
+class Policy_Controller extends Base_Controller {
+    public function action_index() {}
+    public function action_add() {}
+    public function action_edit() {}
+    public function action_delete() {}
+}
+
+?>
