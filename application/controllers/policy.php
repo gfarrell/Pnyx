207e17f6a6054da715cd3b74d07c318a57b1986d
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-25 21:43:21 +0000

    Made sure auth was called before actions

--- application/controllers/policy.php
+++ application/controllers/policy.php
@@ -1,6 +1,10 @@
 <?php
 
 class Policy_Controller extends Base_Controller {
+    public function __construct() {
+        $this->filter('before', 'auth');
+    }
+
     public function action_index() {}
     public function action_add() {}
     public function action_edit() {}
