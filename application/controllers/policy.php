f1b711ea7abfcb9694ac9a5f2fe1be742edc210b
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-27 18:45:44 +0000

    Made controller restful

--- application/controllers/policy.php
+++ application/controllers/policy.php
@@ -1,14 +1,23 @@
 <?php
 
 class Policy_Controller extends Base_Controller {
+    public $restful = true;
+
     public function __construct() {
         $this->filter('before', 'auth');
     }
 
-    public function action_index() {}
-    public function action_add() {}
-    public function action_edit() {}
-    public function action_delete() {}
+    public function get_index() {}
+    public function get_add() {}
+    public function get_edit($id) {
+        $policy = Policy::find($id);
+
+        return View::make('policy.edit')
+                ->with(array(
+                    'policy' => $policy
+                ));
+    }
+    public function delete_delete() {}
 }
 
 ?>
