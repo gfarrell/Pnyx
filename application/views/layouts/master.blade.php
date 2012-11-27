6b42b85c03a736802f080d4b93b8b6b8168d24b4
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-27 21:18:51 +0000

    Added a title to all page layouts

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title><?php { $title = Section::yield('page_title'); echo $title == '' ? '' : $title . ' < '; echo 'PNYX'; } ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
