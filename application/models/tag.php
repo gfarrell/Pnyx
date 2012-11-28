2ee69a33e69c8fdaf3e88549ded424833792c109
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-28 01:42:41 +0000

    Created a method which inserts tags that don't already exist in the db

--- application/models/tag.php
+++ application/models/tag.php
@@ -1,5 +1,18 @@
 <?php
 class Tag extends mBase {
-    
+    public static function createNonexistent($tags) {
+        $tags = array_map(function($t) { return strtolower($t); }, $tags);
+        $existing = Tag::where_in('name', $tags)->get('name');
+
+        $existing = array_map(function($t) { return $t->name; }, $existing);
+
+        $insert = array();
+        foreach($tags as $tag) {
+            if($tag != '' && !in_array($tag, $existing)) {
+                array_push($insert, array('name'=>$tag));
+            }
+        }
+        Tag::insert($insert);
+    }
 }
 ?>
