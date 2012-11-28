5ecebe556e50c4855eeefe4b687def157736eab9
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-28 10:30:16 +0000

    using sparkdown bundle instead of markdown

--- application/views/policy/view.blade.php
+++ application/views/policy/view.blade.php
@@ -4,24 +4,22 @@
     Section::append('page_title', $policy->title);
 ?>
 
-@section('content')
-<h1>$policy->title</h1>
+<?php Section::start('content'); Bundle::start('sparkdown'); ?>
+<h1>{{ $policy->title }}</h1>
 
 <p>Proposed by: {{ $policy->proposed }}<br/>
 Seconded by: {{ $policy->seconded }}</p>
 
 <h3>KCSU Notes</h3>
 
-{{ Markdown($policy->notes) }}
-
+{{ Sparkdown\Markdown($policy->notes) }}
 
 <h3>KCSU Believes</h3>
 
-{{ Markdown($policy->believes) }}
-
+{{ Sparkdown\Markdown($policy->believes) }}
 
 <h3>KCSU Resolves</h3>
 
-{{ Markdown($policy->resolves) }}
+{{ Sparkdown\Markdown($policy->resolves) }}
 
-@endsection
+<?php Section::stop(); ?>
