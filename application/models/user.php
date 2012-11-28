48357ccf8d39be51724ee945f0c816e2dca7fc50
Gideon Farrell <me@gideonfarrell.co.uk>
2012-11-28 01:42:00 +0000

    Added crsid validation and ldap lookup functions

--- application/models/user.php
+++ application/models/user.php
@@ -1,6 +1,16 @@
 <?php
 
 class User extends mBase {
+    public static function validateCrsid($crsid) {
+        return (bool) preg_match('/(\w+)(\d+)/', $crsid);
+    }
+
+    public static function lookup($crsid) {
+        $u = LDAP::search($crsid, 'uid');
+        
+        return count($u) > 0 ? $u[0] : null;
+    }
+
     public function group() {
         return $this->belongs_to('UserGroup');
     }
