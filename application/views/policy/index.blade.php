da6dd33adcb95963d596aed3b2fd0e0cea261b6d
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-16 17:35:09 +0000

    Added a table of policies

--- application/views/policy/index.blade.php
+++ application/views/policy/index.blade.php
@@ -5,4 +5,21 @@
     Section::start('content');
 ?>
 
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Title</th>
+            <th>FOR</th>
+            <th>AGAINST</th>
+            <th>ABSTAIN</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php echo render_each('policy.partials.row', $policies->results, 'policy'); ?>
+    </tbody>
+</table>
+
+<?php echo $policies->links(); ?>
+
 <?php Section::stop(); ?>
