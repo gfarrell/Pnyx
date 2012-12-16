ae54e9a9b5a8a93e84bd82c4ea926c17eab62d9e
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-16 17:36:59 +0000

    Added id to paginated fields

--- application/controllers/policy.php
+++ application/controllers/policy.php
@@ -8,7 +8,7 @@ class Policy_Controller extends Base_Controller {
     }
 
     public function get_index() {
-        $policies = Policy::order_by('date', 'DESC')->paginate(25, array('title','date','votes_for','votes_against','votes_abstain'));
+        $policies = Policy::order_by('date', 'DESC')->paginate(25, array('id', 'title','date','votes_for','votes_against','votes_abstain'));
 
         return Request::ajax() ? Response::eloquent($policies) : View::make('policy.index')->with(array('policies'=>$policies));
     }
