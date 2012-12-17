ad81ed0f864c01a649e8e6d02a6fda0d9d493aa3
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-17 01:04:51 +0000

    Added navbar extras inclusion

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -1,3 +1,6 @@
+<?php
+    $extras = Section::yield('navbar_extras');
+?> 
 <div class="navbar">
     <div class="navbar-inner">
         <a class="brand" href="/">PNYX</a>
@@ -9,6 +12,8 @@
             <li><a href="/policy/add" title="Add a Policy">Add Policy</a></li>
             <li><a href="/admin" title="Administrative Controls">Administration</a></li>
             <?php endif; ?>
+
+            <?php if(!is_null($extras)) { echo $extras; } ?>
         </ul>
         <form class="navbar-search pull-right">
             <input type="text" class="search-query" placeholder="search..." />
