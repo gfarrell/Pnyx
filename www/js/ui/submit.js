c82c6fb362cce255c14a2f413b3db507c4895905
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-20 16:34:50 +0000

    Added automatic assignment of the form submission mechanism

--- www/js/ui/submit.js
+++ www/js/ui/submit.js
@@ -5,16 +5,10 @@ define(
 
         var submit = '[data-submit]',
             Submitter = function(e) {
-                return alert('Hello!');
                 $($(e).data('submit')).submit();
             };
 
-        $.fn.submit = function () {
-            // define plugin so you can use below definition
-        };
-
-        $(function () {
-            $('body').on('click.submit.data-api', submit, Submitter);
-        });
+        // NEED THIS TO BE AUTOMATIC
+        $('body').on('click.submit.data-api', submit, Submitter);
     }
 );
