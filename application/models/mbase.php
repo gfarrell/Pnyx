95bc05763739f5894542cf7aaa8eaa2dce4febc6
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-25 23:12:24 +0000

    Added some saving callbacks beforeSave() and afterSave()

--- application/models/mbase.php
+++ application/models/mbase.php
@@ -1,5 +1,17 @@
 <?php
 class mBase extends Eloquent {
     public static $timestamps = false;
+
+    protected function beforeSave() { return true; }
+    protected function afterSave()  { return true;}
+
+    public function save() {
+        if($this->beforeSave(true)) {
+            parent::save();
+            $this->afterSave(true);
+        } else {
+            throw new Exception('Unable to save data, beforeSave returned false.');
+        }
+    }
 }
 ?>
