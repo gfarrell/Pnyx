236fc1b9df6964e1e08e03b241a5bdcada4c3f22
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-26 01:35:36 +0000

    Made submitter work properly (was non functional before) and also with Bootbox confirmations

--- www/js/ui/submit.js
+++ www/js/ui/submit.js
@@ -1,14 +1,38 @@
 define(
-    ['jquery'],
-    function(jQuery) {
+    ['jquery', 'underscore', 'lib/bootbox/bootbox'],
+    function(jQuery, _, Bootbox) {
         "use strict";
 
         var submit = '[data-submit]',
             Submitter = function(e) {
-                $($(e).data('submit')).submit();
+                e.preventDefault();
+                
+                this.$el = $(e.target);
+
+                if(this.$el.data('confirm')) {
+                    this.getConfirmation();
+                } else {
+                    this.submitForm();
+                }
             };
 
+        _.extend(Submitter.prototype, {
+            getConfirmation: function() {
+                var message = this.$el.data('confirm') || 'Are you sure?';
+
+                Bootbox.confirm(message, function(c) {
+                    if(c) this.submitForm();
+                }.bind(this));
+            },
+
+            submitForm: function() {
+                $(this.$el.data('submit')).submit();
+            }
+        });
+
         // NEED THIS TO BE AUTOMATIC
-        $('body').on('click.submit.data-api', submit, Submitter);
+        $('body').on('click.submit.data-api', submit, function(e) {
+            var s = new Submitter(e);
+        });
     }
 );
