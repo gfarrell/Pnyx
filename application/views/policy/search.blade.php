017cb91d73957bc3cf4ad294567d396364bcbba8
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-26 00:47:21 +0000

    Added a search form with results check and "no results" message

--- application/views/policy/search.blade.php
+++ application/views/policy/search.blade.php
@@ -5,6 +5,22 @@
     Section::start('content');
 ?>
 
+<div class="search">
+<?php
+    $form = Formly::make();
+    echo $form->open('policy/search', 'GET', array('class'=>'form-search'));
+?>
+<div class="input-append">
+    <input type="text" name="query" class="input-xlarge search-query" value="{{ $query }}" placeholder="search..." /><!--
+ --><button type="submit" class="btn icon-search"></button>
+
+</div>
+<?php
+    echo $form->close();
+?>
+</div>
+<?php if(!is_null($query)): ?>
+<?php if(count($policies) > 0): ?>
 <table class="table table-striped table-hover">
     <thead>
         <tr>
@@ -19,5 +35,9 @@
     <?php echo render_each('policy.partials.row', $policies, 'policy'); ?>
     </tbody>
 </table>
+<?php else: ?>
+    @render('partials.alert', array('type'=>'info', 'message'=>'No results'))
+<?php endif; ?>
+<?php endif; ?>
 
 <?php Section::stop(); ?>
