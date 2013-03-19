82b649f39f5297ef70f4a156d814b5b9fa452bea
Gideon Farrell <gideon@Gidbook.local>
2013-03-19 00:55:26 +0000

    Made the current policy page show a table not a list because the list was a bit ugly (and not that consistent)

--- application/views/policy/current.blade.php
+++ application/views/policy/current.blade.php
@@ -4,6 +4,20 @@
     Section::append('page_title', 'Current Policy');
     Section::start('content');
 ?>
-<?php echo render('policy.partials.list', array('policies'=>$policies)); ?>
+<h1>KCSU Current Policy</h1>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Title</th>
+            <th>FOR</th>
+            <th>AGAINST</th>
+            <th>ABSTAIN</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php echo render_each('policy.partials.row', $policies, 'policy'); ?>
+    </tbody>
+</table>
 
 <?php Section::stop(); ?>
