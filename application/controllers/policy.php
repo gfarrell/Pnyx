f05fc975321ef1893e48b8607d2c054f02ffab74
Gideon Farrell <gideon@Gidbook.local>
2013-06-14 16:58:18 +0100

    Use raven:group filter for add, edit, delete

--- application/controllers/policy.php
+++ application/controllers/policy.php
@@ -16,6 +16,7 @@ class Policy_Controller extends Base_Controller {
 
     public function __construct() {
         $this->filter('before', 'raven');
+        $this->filter('before', 'raven:group', array('admin'))->only(array('add', 'edit', 'delete'));
     }
 
     /**
