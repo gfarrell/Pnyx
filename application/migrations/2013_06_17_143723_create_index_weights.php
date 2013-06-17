8bda7f1ebc85fc6f897fffd38e8b656f1ac54764
Gideon Farrell <me@gideonfarrell.co.uk>
2013-06-17 15:38:19 +0100

    Index weights migration
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

---
+++ application/migrations/2013_06_17_143723_create_index_weights.php
@@ -0,0 +1,31 @@
+<?php
+
+class Create_Index_Weights {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('index_weights', function($table){
+			$table->increments('id');
+			$table->string('field_name', 100);
+			$table->integer('multiplier');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('index_weights');
+	}
+
+}
