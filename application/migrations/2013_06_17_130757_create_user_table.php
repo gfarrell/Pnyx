24ea670d0d20efd009cbe1b8f187dee9c21b1a59
Gideon Farrell <me@gideonfarrell.co.uk>
2013-06-17 14:35:04 +0100

    Created a migration to make the user tables
    Create
    * users
    * user_groups
    * user_usergroup
    
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

---
+++ application/migrations/2013_06_17_130757_create_user_table.php
@@ -0,0 +1,52 @@
+<?php
+
+class Create_User_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create users table
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('crsid', 25);	
+		});
+
+		// Create user groups table
+		Schema::create('user_groups', function($table){
+			$table->increments('id');
+			$table->string('name', 50)->unique();
+		});
+
+		// Pivot table
+		Schema::create('user_usergroup', function($table){
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('usergroup_id')->unsigned();
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('usergroup_id')->references('id')->on('user_groups');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop Pivot table
+		Schema::drop('user_usergroup');
+		// Drop groups
+		Schema::drop('user_groups');
+		// Drop users
+		Schema::drop('users');
+	}
+
+}
