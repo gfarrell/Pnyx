7c4149c995c3af960a273a972e562511090e4a7c
Gideon Farrell <me@gideonfarrell.co.uk>
2013-06-17 16:53:18 +0100

    Added SRCF config (sans pwds ;) )
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

---
+++ application/config/srcf/database.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'connections' => array(
+        'mysql' => array(
+            'driver'   => 'mysql',
+            'host'     => 'localhost',
+            'database' => 'kcsu/pnyx',
+            'username' => 'kcsu',
+            'password' => '--enter-password-here--',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        )
+    )
+);
+?>
