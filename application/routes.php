835ce5f820a1157ab41352aa89948383f9b863b0
Gideon Farrell <me@gideonfarrell.co.uk>
2013-06-17 16:33:23 +0100

    Removed tags URL
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- application/routes.php
+++ application/routes.php
@@ -27,15 +27,6 @@ Route::get('/admin', array('before'=>'raven', function() {
 }));
 
 Route::controller('policy');
-Route::get('/tags.json/(:any)', function($query) {
-    $query = Input::get('q');
-    $tags = Tag::where('name', 'LIKE', '%'.$query.'%')->take(10)->get();
-    /*
-        FOR SOME F*ING REASON THIS RETURNS ALL THE RESULTS, EVEN WHEN THE SQL QUERY WORKS PROPERLY
-        WTF!
-     */
-    return Response::eloquent($tags); // Return JSON'd model
-});
 
 /*
 |--------------------------------------------------------------------------
