8b5ca567a90d99346ca7711c6ae2d78ff2fb639f
Gideon Farrell <me@gideonfarrell.co.uk>
2013-06-18 16:58:53 +0100

    Updated user edit page (making it work)
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- application/views/user/edit.blade.php
+++ application/views/user/edit.blade.php
@@ -5,22 +5,20 @@
 ?>
 
 @section('content')
-    <h1>{{ $title . ' ' . $user->crsid }}</h1>
-
     <?php $form = Formly::make($user)->set_options(array('display-inline-errors'=>true)); ?>
     
     {{ $form->open('user/edit/'.$user->id, 'PUT') }}
-    {{ echo $form->hidden('id') }}
+    {{ $form->hidden('id') }}
     
     <fieldset class="row">
-        <legend>Motion Details</legend>
+        <legend>User groups for {{ $user->crsid }}</legend>
         
         <div class="span4 muted">
             <p>A user can belong to any number of groups, and you can select which ones <b>{{ $user->crsid }}</b> belongs to here.</p>
         </div>
         <div class="span8">
             @foreach($groups as $group)
-                {{ $form->checkbox($group->name, $group->id, $user->inGroup($group->name)) }}
+                {{ $form->checkbox('user.groups', $group->name, $group->id, $user->inGroup($group->name)) }}
             @endforeach
         </div>
     </fieldset>
